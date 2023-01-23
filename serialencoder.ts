1fc3ac59650ea7e79b0b5b50cfc8120908a9c3bf
Tomáš Kazda <36702826+TomasKazda@users.noreply.github.com>
2023-01-23 16:31:37 +0100

    negative numbers correction

--- serialencoder.ts
+++ serialencoder.ts
@@ -1,5 +1,6 @@
 //% color="#2a9d8f" icon="\uf162" block="Utility" blockId="UtilityPslib"
 namespace Utility {
+    const ofset = 48;
     /*
     export enum Enum {
         //% block="comment"
@@ -12,17 +13,36 @@ namespace Utility {
     */
     //% blockId=encode_serial block="Get encoded serial number"
     export function encodeSerial(serialNumber?: number): string {
-        const ofset = 48;
         let serialNo = serialNumber === undefined ? control.deviceSerialNumber() : serialNumber;
-        if (serialNumber < 0) {
-            serialNumber = (Math.pow(2, 31)-1) + Math.abs(serialNumber);
+        if (serialNo < 0) {
+            serialNo = (Math.pow(2, 31) - 1) + Math.abs(serialNo);
         }
+        
         let output = "";
         for (let i = 0; serialNo > 0; i++) {
             let fraction = serialNo % Math.pow(64, i+1);
             serialNo -= fraction;
             output = String.fromCharCode(fraction / Math.pow(64, i) + ofset) + output;
         }
-        return output.length < 6 ? String.fromCharCode(ofset) + output : output;
+        return output.length < 6 ? (String.fromCharCode(ofset) + output) : output;
+    }
+
+    /**
+     * Decode serial number
+    */
+    //% blockId=decode_serial block="Decode serial number"
+    export function decodeSerial(encodedSerial: string): number {
+        const ofset = 48;
+        let serialNumber = 0;
+        if (encodedSerial.length === 0) return 0;
+        for (let i = 0; i < encodedSerial.length ; i++) {
+            let charcode = encodedSerial.charCodeAt(i);
+            if (charcode < ofset || charcode > ofset + 64) return 0;
+            serialNumber += (charcode - ofset) * Math.pow(64, encodedSerial.length - (i + 1));
+        }
+        if (serialNumber > (Math.pow(2, 31) - 1)) {
+            serialNumber = (Math.pow(2, 31) - 1) - Math.abs(serialNumber);
+        }
+        return serialNumber
     }
 }
--- test.ts
+++ test.ts
@@ -1,2 +1,4 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+console.log(control.deviceSerialNumber())
 console.log(Utility.encodeSerial())
+console.log(Utility.decodeSerial(Utility.encodeSerial()))
